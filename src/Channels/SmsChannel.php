5b30a5f7d6c8a38e243bc7bc88ac39f7927eab59
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-06-10 05:03:20 -0300

    Atualizando composer dos pacotes

---
+++ src/Channels/SmsChannel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Channels;
+
+use Illuminate\Notifications\Notification;
+use App\Jobs\Sms;
+use Illuminate\Support\Facades\Log;
+
+class SmsChannel
+{
+    /**
+     * Send the given notification.
+     *
+     * @param  mixed  $notifiable
+     * @param  \Illuminate\Notifications\Notification  $notification
+     * @return void
+     */
+    public function send($notifiable, Notification $notification)
+    {
+        $message = $notification->toSms($notifiable);
+        Log::debug(
+            '[NotificationSms] Criando notificação'
+        );
+        // Send notification to the $notifiable instance...
+        Sms::dispatch($message->to, $message->msg);
+    }
+}
